c20b7a635203e95f899832993e13884687394b00
Robert Lemmens <robert_lemmens@hotmail.com>
2014-12-15 19:44:27 +0100

    UI tweaking

--- start.sh
+++ start.sh
@@ -8,12 +8,13 @@ yellow='\033[1;33m'
 NC='\033[0m' # No Color
 
 SAVE_DIR=/home
-TAR_DIR=/
+TAR_DIR=/home
 LOOP_VAR=true
 
 ROOT_UID=0
 E_NOTROOT=87
 
+#zorg dat het script alleen als root uitgevoerd wordt
 if [ "$UID" -ne "$ROOT_UID" ]; then
   echo "Run this script as root"
   exit $E_NOTROOT
@@ -45,10 +46,14 @@ if [ "$USER_CHOICE" = "1" ]; then
   clear
   banner
   echo -e "${yellow}Collector view${white}"
-  echo "Choose a script: "
+  echo ""
+  echo -e "Choose a script: \n"
   echo "1) Grab user info"
   echo "2) Grab system info"
   echo "3) Grab logfiles"
+  echo "4) Back to main menu"
+  echo ""
+  printf "> "
   read USER_IN2
   if [ "$USER_IN2" = "1" ]; then
     bash scripts/grab_userinfo.sh $SAVE_DIR
@@ -56,6 +61,8 @@ if [ "$USER_CHOICE" = "1" ]; then
     bash scripts/grab_sysinfo.sh $SAVE_DIR
   elif [ "$USER_IN2" = "3" ]; then
     bash scripts/grab_logfiles.sh $SAVE_DIR $TAR_DIR/logs.tar
+  elif [ "$USER_IN2" = "4" ]; then
+    echo ""
   fi
   echo "press enter to go back to the main menu"
   read HALT
@@ -63,10 +70,12 @@ elif [ "$USER_CHOICE" = "2" ]; then
   clear
   banner
   echo -e "${yellow}Configuring paths${white}"
-  echo "Enter save path:"
-  read SAVE_DIR
+  echo ""
+  echo -e "Enter save path (e.g:/var/www):\n"
   printf "> "
-  echo "Enter tar path:"
+  read SAVE_DIR
+  echo -e "Enter tar path:(e.g:/var/www):\n"
+  printf "> "
   read TAR_DIR
   printf "> "
 elif [ "$USER_CHOICE" = "3" ]; then
