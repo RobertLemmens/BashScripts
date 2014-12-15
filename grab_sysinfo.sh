f92456d9a8dee9039027a700ad749f2e469745da
Robert Lemmens <robert_lemmens@hotmail.com>
2014-12-15 11:21:34 +0100

    Update in sysinfo

---
+++ grab_sysinfo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+INTER_DIR=/proc/interrupts
+DMA_DIR=/proc/dma
+IOP_DIR=/proc/ioports
+MEM_DIR=/proc/meminfo
+UPTIME_DIR=/proc/uptime
+VERS_DIR=/proc/version
+
+ARRAY=($INTER_DIR $DMA_DIR $IOP_DIR $MEM_DIR $UPTIME_DIR $VERS_DIR)
+
+
+
+echo "Waar wil je het bestand opslaan?"
+
+read SAVE_DIR
+
+echo "Script wordt uitgevoerd..."
+
+touch $SAVE_DIR/sysinfo.txt
+
+for i in 1 2 3 4 5 6
+do
+  echo "" >> $SAVE_DIR/sysinfo.txt
+  echo "-------------------------" >> $SAVE_DIR/sysinfo.txt
+  echo ${ARRAY[$i-1]} " OUTPUT: " >> $SAVE_DIR/sysinfo.txt
+  echo "-------------------------" >> $SAVE_DIR/sysinfo.txt
+  cat ${ARRAY[$i-1]} >> $SAVE_DIR/sysinfo.txt
+done
+
+echo "done"
+
+exit
